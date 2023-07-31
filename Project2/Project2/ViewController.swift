754461cd0cdf8ec9e176b1444aa22e01bbf60025
iiTzAman <aman.giri2055@gmail.com>
2023-07-31 15:54:36 -0400

    API connection

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -6,66 +6,126 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var weatherImage: UIImageView!
     @IBOutlet weak var locationButton: UIButton!
     @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var lblTemperature: UILabel!
+    @IBOutlet weak var lblCityName: UILabel!
     @IBOutlet weak var searchInput: UITextField!
+    @IBOutlet weak var lblWeatherDescription: UILabel!
     let apiKey = "89221bdc6a8c451a9a8212939232807"
     let baseURL = "http://api.weatherapi.com/v1/current.json"
+    var selectedSegmentIndex: Int = 0
+    var locationName = "london"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetchWeather(searchValue: locationName)
+        
         let searchButtonImage = UIImage(systemName: "magnifyingglass")
         searchButton.setImage(searchButtonImage, for: .normal)
         let locationButtonImage = UIImage(systemName: "location.fill")
         locationButton.setImage(locationButtonImage, for: .normal)
         let config = UIImage.SymbolConfiguration(paletteColors: [.systemBlue, .systemYellow])
         self.weatherImage.preferredSymbolConfiguration = config
+        lblCityName.text = locationName
     }
     
     @IBAction func searchClicked(_ sender: Any) {
         let searchValue = searchInput.text
-        if let anything = searchValue{
-            print("search:  \(String(describing: searchValue))")
-            fetchWeather(searchValue: searchValue!)
+        if let search = searchValue {
+            print("search:  \(String(describing: search))")
+            fetchWeather(searchValue: search)
+            print("locaitonName: \(locationName)")
+            
         }
+    }
+    
+    
+    func setUI(locationData: LocationWrapper){
+        locationName = locationData.location.name
+        print("\(locationName)")
+//        lblTemperature.text = String(locationData.current.temp_f)
+    }
+    
+    struct LocationWrapper: Decodable{
+        let location: Location
+        let current: Current
     }
 
-    
-    func fetchWeather(searchValue: String){
-        guard !searchValue.isEmpty else {return}
-        
-        let url = URL( string: "\(baseURL)?key=\(apiKey)&q=\(searchValue)")!
-        
-        print("\(url)")
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-                    if let data = data {
-                        do {
-                            let decoder = JSONDecoder()
-                            decoder.keyDecodingStrategy = .convertFromSnakeCase
-                            let weatherResponse = try decoder.decode(WeatherResponse.self, from: data)
-                            DispatchQueue.main.async {
-                                print(weatherResponse)
-                                self.updateUI(with: weatherResponse.weatherData)
-                            }
-                        } catch {
-                            print("Error decoding JSON: \(error)")
-                        }
-                    }
-                }.resume()
-        
+    struct Location: Decodable {
+        let name: String
+        let region: String
+        let country: String
+        let lat: Float
+        let lon: Float
+    }
+
+    struct Current: Decodable {
+        let temp_c: Float
+        let temp_f: Float
+        let condition: Condition
+    }
+
+    struct Condition: Decodable {
+        let text: String
+        let icon: String
+        let code: Int
     }
     
-    private func updateUI(with weatherData: WeatherData) {
-//            locationLabel.text = "\(weatherData.location.name), \(weatherData.location.region)"
-//            conditionLabel.text = weatherData.current.condition.text
-//            temperatureLabel.text = "\(weatherData.current.tempC)°C"
-        print(" \(weatherData.location.name)")
+    func parseJson (data: Data) -> LocationWrapper?{
+        let decoder = JSONDecoder()
+        var wrapper: LocationWrapper?
+        
+        do{
+            wrapper = try decoder.decode(LocationWrapper.self, from: data)
+            
+        }catch{
+            print(error)
         }
+        return wrapper
+    }
+    
+    @IBAction func selectedTempType(_ sender: UISegmentedControl) {
+        selectedSegmentIndex = sender.selectedSegmentIndex
+        print("Selected segment index: \(selectedSegmentIndex)")
+    }
+    
+    func fetchWeather(searchValue: String) {
+        guard !searchValue.isEmpty else {return}
+        
+        let url: URL? = URL( string: "\(baseURL)?key=\(apiKey)&q=\(searchValue)")!
+        let urlSession = URLSession(configuration: .default)
+        
+        if let url = url {
+            let dataTask = urlSession.dataTask(with: url) {
+                data, response, error in
+                
+                guard error == nil else {
+                    print (error ?? "")
+                    return
+                }
+                
+                guard let data = data else {
+                    print("No data")
+                    return
+                }
+                
+                if let locationData = self.parseJson(data: data){
+                    print(locationData)
+                    self.setUI(locationData: locationData)
+                }
+            }
+            dataTask.resume()
+        }
+    }
     
 }
+  
+    
 
